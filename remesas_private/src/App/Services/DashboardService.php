6b268d17d0d11a990ac8428d42706b49c4ccd52f
Nicolas-Salas <nicolas.salas.contacto@gmail.com>
2025-10-31 20:14:42 -0300

    Fix: Error Dashboard
    Se arregla error en el dashboard del admin, se cambia caracter especial por ó en el servidor CPanel

--- remesas_private/src/App/Services/DashboardService.php
+++ remesas_private/src/App/Services/DashboardService.php
@@ -39,7 +39,7 @@ class DashboardService
     {
         $id = $this->estadoTxRepo->findIdByName($nombreEstado);
         if ($id === null) {
-            throw new Exception("Configuración interna: Estado de transacción '{$nombreEstado}' no encontrado.", 500);
+            throw new Exception("Configuraci車n interna: Estado de transacci車n '{$nombreEstado}' no encontrado.", 500);
         }
         return $id;
     }
